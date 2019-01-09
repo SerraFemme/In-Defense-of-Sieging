9ce0f9cc2b44cd0b5759955d9ef5350c28178ce7
buckneruss <buckneruss@yahoo.com>
2019-01-09 16:00:28 -0600

    Beginning of the Game
    Starting list of JSON Files needed for the game and a few Python methods
    that can read then dump the data of those JSON files

---
+++ PythonApplication1/PythonApplication1/ReadCardL.py
@@ -0,0 +1,25 @@
+
+# testing how to use json in python
+import json # allows Python to use JSON
+import os
+
+#list directory contents
+#print(os.listdir())
+
+#List current working directory
+print("Current Working Directory " , os.getcwd())
+
+try:
+    #Change directory to the one I need to
+    os.chdir("D:\\3 My New Game\\PythonGameCode\\PythonApplication1\\PythonApplication1\\JSON Files")
+
+except:
+    print("Can't change the Current Working Directory") 
+
+#test line of code
+#with open('EquipmentLibrary.json') as EquipL: data = json.load(EquipL)
+
+#get Equipment Library and print to screen
+with open('CardLibrary.json') as CardL:
+    data = json.load(CardL)
+print(json.dumps(data, indent=4))
