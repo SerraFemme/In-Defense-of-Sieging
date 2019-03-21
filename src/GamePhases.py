e490293b101939c69c645be72a15393bdba3e4f3
buckneruss <buckneruss@yahoo.com>
2019-03-21 10:58:46 -0500

    Revert "Revert "More work on Enemy Team""
    This reverts commit 5d77683bb414a102e8e920f629d40bce26747780.

--- src/GamePhases.py
+++ src/GamePhases.py
@@ -120,7 +120,10 @@ class PlayerTurn(object):  # TODO: Update to new system
 
 class EnemyTurn(object):  # TODO: Update to new system
     """
-    Controls the enemy during their phase
+    Controls the enemy during their phase.
+    Enemies will always try to move first.
+    After movement, enemies will try to take the best action(s).
+    Then the enemy will pass their turn.
     """
 
     def __init__(self, battle_map, player_team):
